5deb462593e733296609ad9037f97dd732358765
vloss <viniciusvianaloss@gmail.com>
2023-03-08 23:10:02 -0300

    Inclusão de projeto disponibilizado.

---
+++ MeusOlhos/ObjectsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ObjectsViewController.swift
+//  MeusOlhos
+//
+//  Created by Eric Brito
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class ObjectsViewController: UIViewController {
+    
+    @IBOutlet weak var viCamera: UIView!
+    @IBOutlet weak var lbIdentifier: UILabel!
+    @IBOutlet weak var lbConfidence: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func analyse(_ sender: UIButton) {
+    }
+}

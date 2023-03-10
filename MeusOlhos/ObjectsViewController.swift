686f2dc4830941c886c72f845166dc3ef61c8ddc
vloss <viniciusvianaloss@gmail.com>
2023-03-10 01:03:51 -0300

    Criação da extension para que o modelo de dados analise a imagem capturada.

--- MeusOlhos/ObjectsViewController.swift
+++ MeusOlhos/ObjectsViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
 class ObjectsViewController: UIViewController {
     
@@ -14,10 +16,39 @@ class ObjectsViewController: UIViewController {
     @IBOutlet weak var lbIdentifier: UILabel!
     @IBOutlet weak var lbConfidence: UILabel!
     
+    lazy captureManager: CaptureManager = {
+        let captureManager = CaptureManager()
+        captureManager.videoBufferDelegate = self
+        return captureManager
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func analyse(_ sender: UIButton) {
+    }
+}
+
+extension ObjectsViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let model = try? VNCoreMLModel(for: VGG16().model) else { return }
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {return}
+            for i in 0...5 {
+                print(results[i].identifier, results[i].confidence)
+            }
+            print("=============================================")
+            guard let firstObservation = results.first else {return}
+            
+            DispatchQueue.main.async {
+                lbIdentifier.text = firstObservation.identifier
+                
+                let confidence = round(firstObservation.confidence*1000) / 10
+                lbConfidence.text = "\(confidence)%"
+            }
+            
+        }
     }
 }
